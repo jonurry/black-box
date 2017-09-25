736132e1ac50a34eb91ae38a939a8e881638cb9f
Jon Urry <github@agileweb.co.uk>
2017-09-25 18:32:13 +0100

    Shoot rays.

--- js/blackbox.js
+++ js/blackbox.js
@@ -6,10 +6,15 @@ var LOCATION_TYPE = {
 };
 
 var SHOOT_RAY_OUTCOME = {
-  ABSORBED: 'absorbed',
-  NOTHING: 'nothing',
-  PROPOGATED: 'propogated',
-  REFLECTED: 'reflected'
+  ABSORBED: 'ray hit marble and was absorbed',
+  CORNER: 'ray is in a corner',
+  DUPLICATE: 'ray has already been shot',
+  MARBLE_PLACED: 'marble has been placed',
+  MARBLE_REMOVED: 'marble has been removed',
+  NOTHING: 'no outcome',
+  OUTSIDE: 'ray is outside of the black box',
+  PROPOGATED: 'ray has reached the rim',
+  REFLECTED: 'ray has been reflected'
 }
 
 function BlackBox(gridSize = 8, numberOfMarbles = 4) {
@@ -18,6 +23,69 @@ function BlackBox(gridSize = 8, numberOfMarbles = 4) {
   this.guesses= [];
   this.numberOfMarbles = numberOfMarbles;
   this.numberOfRays = 0;
+  this.checkForDeflectedRay = function(ray) {
+    var probe1 = 0;
+    var probe2 = 0;
+    var row = ray.getPosition().row;
+    var column = ray.getPosition().column;
+    var gridUpperBound = this.gridSize + 1;
+    if (ray.direction === DIRECTION.UP || ray.direction === DIRECTION.DOWN) {
+      if(column + 1 === gridUpperBound) {
+        probe1 = 0;
+      } else {
+        probe1 = this.grid[row][column + 1];
+      }
+      if(column - 1 === 0) {
+        probe2 = 0;
+      } else {
+        probe2 = this.grid[row][column - 1];
+      }
+      if (probe1 === 1 && probe2 === 1) {
+        //two adjacent marbles so ray is reversed
+        ray.direction = (ray.direction === DIRECTION.UP) ? DIRECTION.DOWN : DIRECTION.UP;
+        ray.move();
+      } else if (probe1 === 1) {
+        // one adjacent marble so go back one space and then head LEFT
+        ray.direction = (ray.direction === DIRECTION.UP) ? DIRECTION.DOWN : DIRECTION.UP;
+        ray.move();
+        ray.direction = DIRECTION.LEFT;
+      } else if (probe2 === 1) {
+        // one adjacent marble so go back one space and then head RIGHT
+        ray.direction = (ray.direction === DIRECTION.UP) ? DIRECTION.DOWN : DIRECTION.UP;
+        ray.move();
+        ray.direction = DIRECTION.RIGHT;
+      }
+    } else {
+      //ray is travelling LEFT or RIGHT
+      // ignore case where first propogated ray is encoutered at edge
+      if(row + 1 === gridUpperBound) {
+        probe1 = 0;
+      } else {
+        probe1 = this.grid[row + 1][column];
+      }
+      if(row - 1 === 0) {
+        probe2 = 0;
+      } else {
+        probe2 = this.grid[row - 1][column];
+      }
+      if (probe1 === 1 && probe2 === 1) {
+        //two adjacent marbles so ray is reversed
+        ray.direction = (ray.direction === DIRECTION.LEFT) ? DIRECTION.RIGHT : DIRECTION.LEFT;
+        ray.move();
+      } else if (probe1 === 1) {
+        // one adjacent marble so go back one space and then head UP
+        ray.direction = (ray.direction === DIRECTION.LEFT) ? DIRECTION.RIGHT : DIRECTION.LEFT;
+        ray.move();
+        ray.direction = DIRECTION.UP;
+      } else if (probe2 === 1) {
+        // one adjacent marble so go back one space and then head DOWN
+        ray.direction = (ray.direction === DIRECTION.LEFT) ? DIRECTION.RIGHT : DIRECTION.LEFT;
+        ray.move();
+        ray.direction = DIRECTION.DOWN;
+      }
+    }
+
+  };
   this.createGrid = function() {
     //first row. 2 extra slots to record ray outcomes at either end
     this.grid = new Array(this.gridSize + 2);
@@ -54,15 +122,18 @@ function BlackBox(gridSize = 8, numberOfMarbles = 4) {
   this.guess = function(newGuess) {
     var removeGuess = -1;
     for (var i = 0, guess; guess = this.guesses[i]; i++) {
-      if (guess.row === newGuess.row && guess.column === newGuess.column) {
+      if (guess.row === newGuess.getPosition().row &&
+          guess.column === newGuess.getPosition().column) {
         // that guess has already been made so remove guess
         removeGuess = i;
       }
     }
     if (removeGuess > -1) {
       this.guesses.splice(removeGuess, 1);
+      return SHOOT_RAY_OUTCOME.MARBLE_REMOVED;
     } else if (this.guesses.length < this.numberOfMarbles) {
-      this.guesses.push(newGuess);
+      this.guesses.push(newGuess.getPosition());
+      return SHOOT_RAY_OUTCOME.MARBLE_PLACED;
     }
   };
   this.initialiseGrid = function() {
@@ -81,6 +152,52 @@ function BlackBox(gridSize = 8, numberOfMarbles = 4) {
       this.grid[x][y] = 1;
     }
   };
+  this.rayAlreadyShot = function(ray) {
+    // check if ray has already been shot from current location (i.e. duplicate)
+    return (this.grid[ray.getPosition().row][ray.getPosition().column] !== 0);
+  };
+  this.rayHasHitMarble = function(ray) {
+    // check if ray has hit a marble
+    return (this.grid[ray.getPosition().row][ray.getPosition().column] === 1);
+  };
+  this.rayHasRechedRim = function(ray) {
+    var currentRow = ray.getPosition().row;
+    var currentColumn = ray.getPosition().column;
+    var gridUpperBound = this.gridSize + 1;
+    // check if ray has reached the rim
+    return (currentRow === 0 ||
+            currentRow === gridUpperBound ||
+            currentColumn === 0 ||
+            currentColumn === gridUpperBound);
+  };
+  this.rayIsInCorner = function(ray) {
+    var currentRow = ray.getPosition().row;
+    var currentColumn = ray.getPosition().column;
+    var gridUpperBound = this.gridSize + 1;
+    // check if ray is in a corner
+    return ((currentRow === 0 && currentColumn === 0) ||
+            (currentRow === 0 && currentColumn === gridUpperBound) ||
+            (currentRow === gridUpperBound && currentColumn === 0) ||
+            (currentRow === gridUpperBound && currentColumn === gridUpperBound));
+  };
+  this.rayIsInsideGrid = function(ray) {
+    var currentRow = ray.getPosition().row;
+    var currentColumn = ray.getPosition().column;
+    var gridUpperBound = this.gridSize + 1;
+    return (currentRow > 0 &&
+            currentColumn > 0 &&
+            currentRow < gridUpperBound &&
+            currentColumn < gridUpperBound);
+  };
+  this.rayIsOutsideGrid = function(ray) {
+    var currentRow = ray.getPosition().row;
+    var currentColumn = ray.getPosition().column;
+    var gridUpperBound = this.gridSize + 1;
+    return (currentRow < 0 ||
+            currentColumn < 0 ||
+            currentRow > gridUpperBound ||
+            currentColumn > gridUpperBound);
+  };
   this.renderGrid = function() {
     var gridLine;
     for (i = 0; i < this.grid.length; i++) {
@@ -93,152 +210,51 @@ function BlackBox(gridSize = 8, numberOfMarbles = 4) {
     }
   };
   this.shootRay = function(ray) {
-    return SHOOT_RAY_OUTCOME.ABSORBED;
+    var originalRow = ray.getPosition().row;
+    var originalColumn = ray.getPosition().column;
+    var outcome = SHOOT_RAY_OUTCOME.NOTHING;
+    if (this.rayIsOutsideGrid(ray)) {
+      outcome = SHOOT_RAY_OUTCOME.OUTSIDE;
+    } else if (this.rayIsInCorner(ray)) {
+      outcome = SHOOT_RAY_OUTCOME.CORNER;
+    } else if (this.rayIsInsideGrid(ray)){
+      outcome = this.guess(ray);
+    } else if (this.rayAlreadyShot(ray)) {
+      outcome = SHOOT_RAY_OUTCOME.DUPLICATE;
+    } else {
+      do {
+        ray.move();
+        if (this.rayHasHitMarble(ray)) {
+          this.grid[originalRow][originalColumn] = 'a';
+          outcome = SHOOT_RAY_OUTCOME.ABSORBED;
+        } else if (this.rayHasRechedRim(ray)) {
+          if (ray.getPosition().row === originalRow &&
+              ray.getPosition().column === originalColumn) {
+            this.grid[originalRow][originalColumn] = 'r';
+            outcome = SHOOT_RAY_OUTCOME.REFLECTED;
+          } else {
+            this.numberOfRays += 1;
+            this.grid[ray.getPosition().row][ray.getPosition().column] = this.numberOfRays;
+            this.grid[originalRow][originalColumn] = this.numberOfRays;
+            outcome = SHOOT_RAY_OUTCOME.PROPOGATED;
+          }
+        } else {
+          this.checkForDeflectedRay(ray);
+          if (this.rayHasRechedRim(ray)) {
+            if (ray.getPosition().row === originalRow &&
+                ray.getPosition().column === originalColumn) {
+              this.grid[originalRow][originalColumn] = 'r';
+              outcome = SHOOT_RAY_OUTCOME.REFLECTED;
+            } else {
+              this.numberOfRays += 1;
+              this.grid[ray.getPosition().row][ray.getPosition().column] = this.numberOfRays;
+              this.grid[originalRow][originalColumn] = this.numberOfRays;
+              outcome = SHOOT_RAY_OUTCOME.PROPOGATED;
+            }
+          }
+        }
+      } while (outcome === SHOOT_RAY_OUTCOME.NOTHING)
+    }
+    return outcome;
   };
-  this.shootRayOriginal = function(x, y) {
-
-    // initialise ray position
-    var xPos = x;
-    var yPos = y;
-    var gridUpperBound = this.gridSize + 1;
-
-    // ignore shots from corners of the grid
-    if ((x === 0 && y === 0) ||
-        (x === 0 && y === gridUpperBound) ||
-        (x === gridUpperBound && y === 0) ||
-        (x === gridUpperBound && y === gridUpperBound)) {
-      console.log('corner');
-      return;
-    }
-
-    // guess marble location inside the grid
-    if (x > 0 && y > 0 && x < gridUpperBound && y < gridUpperBound) {
-      this.guess(x , y);
-      console.log(this.guesses);
-      return;
-    }
-
-    // ignore duplicate rim shots
-    if (this.grid[x][y] !== 0) {
-      console.log('duplicate shot');
-      return;
-    }
-
-    // get direction of travel
-    var xDir = 0;
-    var yDir = 0;
-    if (x === 0) {
-      xDir = 1;
-    } else if (x === (gridUpperBound)) {
-      xDir = -1;
-    }
-    if (y === 0) {
-      yDir = 1;
-    } else if (y === (gridUpperBound)) {
-      yDir = -1;
-    }
-
-    do {
-
-      var probe1, probe2;
-
-      // set next ray position
-      xPos = xPos + xDir;
-      yPos = yPos + yDir;
-
-      console.log('position:', xPos, yPos);
-      console.log('direction:', xDir, yDir);
-
-      //test for a direct hit
-      if (this.grid[xPos][yPos] === 1) {
-        console.log('hit');
-        this.grid[x][y] = 'a';
-        this.renderGrid();
-        return;
-      }
-
-      // test if edge of grid has been reached
-      if (xPos === 0 || yPos === 0 || xPos === gridUpperBound || yPos === gridUpperBound) {
-        console.log('progogation');
-        this.numberOfRays += 1;
-        this.grid[x][y] = this.numberOfRays;
-        this.grid[xPos][yPos] = this.numberOfRays;
-        this.renderGrid();
-        return;
-      }
-
-      //test for reflection
-      if (xDir === 0) {
-        //ray is travelling in the y-axis so test for marble presence in the adjacent x-axis locations
-        // ignore case where first propogated ray is encoutered at edge
-        if(xPos + 1 === gridUpperBound) {
-          probe1 = 0;
-        } else {
-          probe1 = this.grid[xPos + 1][yPos];
-        }
-        if(xPos - 1 === 0) {
-          probe2 = 0;
-        } else {
-          probe2 = this.grid[xPos - 1][yPos];
-        }
-        if (probe1 === 1 && probe2 === 1) {
-          //two adjacent marbles so ray is reflected
-          console.log('reflection');
-          this.grid[x][y] = 'r';
-          this.renderGrid();
-          return;
-        } else if (probe1 === 1) {
-          // one adjacent marble so change position and direction
-          yPos -= yDir;
-          xDir = -1;
-          yDir = 0;
-        } else if (probe2 === 1) {
-          // one adjacent marble so change position and direction
-          yPos -= yDir;
-          xDir = 1;
-          yDir = 0;
-        }
-      } else if (yDir === 0) {
-        //ray is travelling in the x-axis so test for marble presence in the adjacent y-axis locations
-        // ignore case where first propogated ray is encoutered at edge
-        if(yPos + 1 === gridUpperBound) {
-          probe1 = 0;
-        } else {
-          probe1 = this.grid[xPos][yPos + 1];
-        }
-        if(yPos - 1 === 0) {
-          probe2 = 0;
-        } else {
-          probe2 = this.grid[xPos][yPos - 1];
-        }
-        if (probe1 === 1 && probe2 === 1) {
-          //two adjacent marbles so ray is reflected
-          console.log('reflection');
-          this.grid[x][y] = 'r';
-          this.renderGrid();
-          return;
-        } else if (probe1 === 1) {
-          // one adjacent marble so change position and direction
-          xPos -= xDir;
-          xDir = 0;
-          yDir = -1;
-        } else if (probe2 === 1) {
-          // one adjacent marble so change position and direction
-          xPos -= xDir;
-          xDir = 0;
-          yDir = 1;
-        }
-      }
-
-    } while (xPos > 0 && yPos > 0 && xPos < gridUpperBound && yPos < gridUpperBound)
-
-    // check for immediate reflection
-    if (xPos === x && yPos === y) {
-      console.log('reflection');
-      this.grid[x][y] = 'r';
-    }
-
-    this.renderGrid();
-
-  }
 };
--- tests/blackbox.test.js
+++ tests/blackbox.test.js
@@ -163,9 +163,9 @@ describe('It should handle guesses as to the locations of the marbles in the Bla
     var blackbox = new BlackBox(8, 4);
     // Create 2 Vectors and add them as guesses
     var guess1 = new Vector(3, 4);
-    blackbox.guess(guess1.getPosition());
+    blackbox.guess(guess1);
     var guess2 = new Vector(7, 2);
-    blackbox.guess(guess2.getPosition());
+    blackbox.guess(guess2);
     expect(blackbox.guesses.length).toBe(2);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     expect(blackbox.guesses[1]).toEqual(guess2.getPosition());
@@ -174,17 +174,17 @@ describe('It should handle guesses as to the locations of the marbles in the Bla
   test('It should accept no more guesses than there are marbles in the game.', () => {
     var blackbox = new BlackBox(8, 4);
     var guess1 = new Vector(1, 1);
-    blackbox.guess(guess1.getPosition());
+    blackbox.guess(guess1);
     var guess2 = new Vector(2, 2);
-    blackbox.guess(guess2.getPosition());
+    blackbox.guess(guess2);
     var guess3 = new Vector(3, 3);
-    blackbox.guess(guess3.getPosition());
+    blackbox.guess(guess3);
     var guess4 = new Vector(4, 4);
-    blackbox.guess(guess4.getPosition());
+    blackbox.guess(guess4);
     var guess5 = new Vector(5, 5);
-    blackbox.guess(guess5.getPosition());
+    blackbox.guess(guess5);
     var guess6 = new Vector(6, 6);
-    blackbox.guess(guess6.getPosition());
+    blackbox.guess(guess6);
     expect(blackbox.guesses.length).toBe(4);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     expect(blackbox.guesses[1]).toEqual(guess2.getPosition());
@@ -196,43 +196,88 @@ describe('It should handle guesses as to the locations of the marbles in the Bla
     var blackbox = new BlackBox(8, 4);
     // first 3 guesses are different so all are added in order
     var guess1 = new Vector(1, 1);
-    blackbox.guess(guess1.getPosition());
+    blackbox.guess(guess1);
     expect(blackbox.guesses.length).toBe(1);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     var guess2 = new Vector(2, 2);
-    blackbox.guess(guess2.getPosition());
+    blackbox.guess(guess2);
     expect(blackbox.guesses.length).toBe(2);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     expect(blackbox.guesses[1]).toEqual(guess2.getPosition());
     var guess3 = new Vector(3, 3);
-    blackbox.guess(guess3.getPosition());
+    blackbox.guess(guess3);
     expect(blackbox.guesses.length).toBe(3);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     expect(blackbox.guesses[1]).toEqual(guess2.getPosition());
     expect(blackbox.guesses[2]).toEqual(guess3.getPosition());
     // next guess is duplicate of second guess so second guess is removed leaving guesses one and three.
     var guess4 = new Vector(2, 2);
-    blackbox.guess(guess4.getPosition());
+    blackbox.guess(guess4);
     expect(blackbox.guesses.length).toBe(2);
     expect(blackbox.guesses[0]).toEqual(guess1.getPosition());
     expect(blackbox.guesses[1]).toEqual(guess3.getPosition());
     // next guess is duplicate of first guess so first guess is removed leaving only third guess.
     var guess5 = new Vector(1, 1);
-    blackbox.guess(guess5.getPosition());
+    blackbox.guess(guess5);
     expect(blackbox.guesses.length).toBe(1);
     expect(blackbox.guesses[0]).toEqual(guess3.getPosition());
     // final guess is a duplicate of third guess so thired guess us removed. There are no guesses left.
     var guess6 = new Vector(3, 3);
-    blackbox.guess(guess6.getPosition());
+    blackbox.guess(guess6);
     expect(blackbox.guesses.length).toBe(0);
   });
 
 });
 
-describe('It should shoot rays from the rim.', () => {
+describe('It should ignore rays shot from the corners or from outside.', () => {
 
-//  beforeAll(() => {
-  //});
+  test('It should ignore shots from the corners.', () => {
+    var blackbox = new BlackBox(8, 4);
+    blackbox.grid[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[2] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[3] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[4] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+    blackbox.grid[5] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+    blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(blackbox.shootRay(new Vector(0, 0, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.CORNER);
+    expect(blackbox.shootRay(new Vector(0, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.CORNER);
+    expect(blackbox.shootRay(new Vector(9, 9, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.CORNER);
+    expect(blackbox.shootRay(new Vector(9, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.CORNER);
+  });
+
+  test('It should ignore shots from ouside the black box.', () => {
+    var blackbox = new BlackBox(8, 4);
+    blackbox.grid[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[2] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[3] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[4] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+    blackbox.grid[5] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+    blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(blackbox.shootRay(new Vector(-1, -1))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(-1, 5))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(-1, 10))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(10, -1))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(10, 5))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(10, 10))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(-1, -1))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(5, -1))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(10, -1))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(-1, 10))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(5, 10))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+    expect(blackbox.shootRay(new Vector(10, 10))).toBe(SHOOT_RAY_OUTCOME.OUTSIDE);
+  });
+
+});
+
+describe('It should shoot rays from the rim.', () => {
 
   test('It should absorb rays shot at marbles.', () => {
     var blackbox = new BlackBox(8, 4);
@@ -247,25 +292,106 @@ describe('It should shoot rays from the rim.', () => {
     blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
     blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     expect(blackbox.shootRay(new Vector(0, 1, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[0][1]).toBe('a');
     expect(blackbox.shootRay(new Vector(0, 6, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
-    expect(blackbox.shootRay(new Vector(0, 7, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[0][6]).toBe('a');
     expect(blackbox.shootRay(new Vector(0, 8, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[0][8]).toBe('a');
     expect(blackbox.shootRay(new Vector(1, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[1][9]).toBe('a');
     expect(blackbox.shootRay(new Vector(4, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[4][9]).toBe('a');
     expect(blackbox.shootRay(new Vector(8, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[8][9]).toBe('a');
     expect(blackbox.shootRay(new Vector(9, 1, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
-    expect(blackbox.shootRay(new Vector(9, 2, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[9][1]).toBe('a');
     expect(blackbox.shootRay(new Vector(9, 6, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[9][6]).toBe('a');
     expect(blackbox.shootRay(new Vector(9, 8, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[9][8]).toBe('a');
     expect(blackbox.shootRay(new Vector(1, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[1][0]).toBe('a');
     expect(blackbox.shootRay(new Vector(3, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[3][0]).toBe('a');
     expect(blackbox.shootRay(new Vector(8, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[8][0]).toBe('a');
   });
 
-});
+  test('It should deflect rays shot past marbles', () => {
+    var blackbox = new BlackBox(8, 4);
+    blackbox.grid[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[2] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[3] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[4] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+    blackbox.grid[5] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+    blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(blackbox.shootRay(new Vector(0, 7, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[0][7]).toBe('a');
+    expect(blackbox.shootRay(new Vector(9, 2, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.ABSORBED);
+    expect(blackbox.grid[9][2]).toBe('a');
+    expect(blackbox.shootRay(new Vector(7, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.PROPOGATED);
+    expect(blackbox.grid[7][9]).toBe(1);
+    expect(blackbox.grid[5][0]).toBe(1);
+    expect(blackbox.shootRay(new Vector(7, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.PROPOGATED);
+    expect(blackbox.grid[7][0]).toBe(2);
+    expect(blackbox.grid[0][5]).toBe(2);
+  });
 
-test.skip('It should set the current ray position by row and column number', () => {
-  var pos = [2, 4];
-  blackbox.setRayPosition(pos);
-  expect(blackbox.rayPosition).toBe(pos);
+  test('It should reflect rays', () => {
+    var blackbox = new BlackBox(8, 4);
+    blackbox.grid[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[2] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[3] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[4] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+    blackbox.grid[5] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+    blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(blackbox.shootRay(new Vector(0, 2, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[0][2]).toBe('r');
+    expect(blackbox.shootRay(new Vector(2, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[2][9]).toBe('r');
+    expect(blackbox.shootRay(new Vector(3, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[3][9]).toBe('r');
+    expect(blackbox.shootRay(new Vector(5, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[5][9]).toBe('r');
+    expect(blackbox.shootRay(new Vector(9, 5, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[9][5]).toBe('r');
+    expect(blackbox.shootRay(new Vector(9, 7, DIRECTION.UP))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[9][7]).toBe('r');
+    expect(blackbox.shootRay(new Vector(2, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[2][0]).toBe('r');
+    expect(blackbox.shootRay(new Vector(4, 0, DIRECTION.RIGHT))).toBe(SHOOT_RAY_OUTCOME.REFLECTED);
+    expect(blackbox.grid[4][0]).toBe('r');
+  });
+
+  test('It should propogate rays without reflection', () => {
+    var blackbox = new BlackBox(8, 4);
+    blackbox.grid[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[2] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[3] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[4] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+    blackbox.grid[5] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    blackbox.grid[8] = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+    blackbox.grid[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(blackbox.shootRay(new Vector(0, 3, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.PROPOGATED);
+    expect(blackbox.grid[0][3]).toBe(1);
+    expect(blackbox.grid[9][3]).toBe(1);
+    expect(blackbox.shootRay(new Vector(0, 4, DIRECTION.DOWN))).toBe(SHOOT_RAY_OUTCOME.PROPOGATED);
+    expect(blackbox.grid[0][4]).toBe(2);
+    expect(blackbox.grid[9][4]).toBe(2);
+    expect(blackbox.shootRay(new Vector(6, 9, DIRECTION.LEFT))).toBe(SHOOT_RAY_OUTCOME.PROPOGATED);
+    expect(blackbox.grid[6][9]).toBe(3);
+    expect(blackbox.grid[6][0]).toBe(3);
+  });
+
 });
